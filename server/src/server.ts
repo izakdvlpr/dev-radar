628db6c5ba28c4c97bc7887956cee957a7f05e58
zevdvlpr <zevdvlpr@gmail.com>
2020-09-10 12:24:19 -0300

    fix: edit port

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,4 +2,4 @@ import 'dotenv/config';
 
 import app from "./app";
 
-app.listen(3333, () => console.log('> Servidor rodando na porta: "3333"'));
+app.listen(process.env.PORT, () => console.log('> Servidor rodando na porta: "3333"'));
